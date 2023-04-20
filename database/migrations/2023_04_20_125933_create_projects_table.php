478450705ebfaf93ac11b23b1488c20ecdd8319d
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-20 15:27:37 +0200

    FEAT correct table

--- database/migrations/2023_04_20_125933_create_projects_table.php
+++ database/migrations/2023_04_20_125933_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('slug');
+            $table->date('date_creation');
             $table->timestamps();
         });
     }
