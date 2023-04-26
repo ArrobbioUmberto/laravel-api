e0be7d62279992917731772460d11b8651e22c31
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-26 16:17:39 +0200

    FIX  error in slug

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -21,7 +21,7 @@ class TypeSeeder extends Seeder
         foreach ($type as $type_name) {
             $new_type = new Type();
             $new_type->name = $type_name;
-            $new_type->Str::slug($type_name);
+            $new_type->slug = Str::slug($type_name);
 
             $new_type->save();
         }
